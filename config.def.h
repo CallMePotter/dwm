ea80e31b8387e7d07f0b82a5d4082cc1b8c42d59
Dmitriy Potter <dmitriypottero@gmail.com>
2020-10-05 15:12:26 +0500

    st on other monitor

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -36,8 +36,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            1,          -1 },
+	{ "Firefox",  NULL,       NULL,       1,            0,          -1 },
+	{ "st",       NULL,       NULL,       1,            0,           1 },
 };
 
 /* layout(s) */
