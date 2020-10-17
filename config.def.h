eb57154cd606873344e18db15d211582bd908377
Dmitriy Potter <dmitriypottero@gmail.com>
2020-10-17 19:06:24 +0500

    toggle bar

--- config.def.h
+++ config.def.h
@@ -84,7 +84,7 @@ static Key keys[] = {
 
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
